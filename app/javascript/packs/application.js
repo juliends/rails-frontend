4d86bf6c521266f89ca3aa9c688a0e0ed8bf4b5c
julien da silva <julien.da.silva@hotmail.com>
2019-08-15 10:46:51 +0200

    lecture done

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from '../components/banner';
+
+initUpdateNavbarOnScroll();
+loadDynamicBannerText();
